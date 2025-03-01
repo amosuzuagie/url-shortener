b3b481bd80ff7760183c0a675230fb1675229dab
Amos Uzuagie <amosuzuagie@gmail.com>
2025-03-01 07:20:16 +0100

    Creates Registration and Login features. Updates WebSecurityConfig class.

--- url-shortener-backend/src/main/java/com/url/shortener/security/jwt/JwtAuthenticationResponse.java
+++ url-shortener-backend/src/main/java/com/url/shortener/security/jwt/JwtAuthenticationResponse.java
@@ -5,4 +5,8 @@ import lombok.Data;
 @Data
 public class JwtAuthenticationResponse {
     private String token;
+
+    public JwtAuthenticationResponse(String token) {
+        this.token = token;
+    }
 }
---
+++ url-shortener-backend/src/main/java/com/url/shortener/service/UserService.java
@@ -0,0 +1,39 @@
+package com.url.shortener.service;
+
+import com.url.shortener.dtos.LoginRequest;
+import com.url.shortener.models.User;
+import com.url.shortener.repository.UserRepository;
+import com.url.shortener.security.jwt.JwtAuthenticationResponse;
+import com.url.shortener.security.jwt.JwtUtils;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private PasswordEncoder passwordEncoder;
+    private UserRepository userRepository;
+    private JwtUtils jwtUtils;
+    private AuthenticationManager authenticationManager;
+
+    public void registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+    public JwtAuthenticationResponse authenticateUser(LoginRequest request) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        String jwt = jwtUtils.generateToken(userDetails);
+        return new JwtAuthenticationResponse(jwt);
+    }
+}
